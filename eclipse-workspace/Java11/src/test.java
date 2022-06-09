2e015ca688dfe628f1977da7f3e9b810a4823304
nghiangong <nmn16102002@gmail.com>
2022-06-10 00:21:04 +0700

    test status

--- eclipse-workspace/Java11/src/test.java
+++ eclipse-workspace/Java11/src/test.java
@@ -13,5 +13,6 @@ public class test {
 		System.out.println("Gia trij nhap la: "+n);
 		System.out.println("Ket thuc chuong trinh.");
 		System.out.println("test push");
+		System.out.println("teststatus");
 }
 }
